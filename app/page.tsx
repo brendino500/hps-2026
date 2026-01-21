af8479b9e1bba6c70fa5be3f1ebf8061199bebe1
brendino500 <brenda.ty@live.com>
2026-01-21 11:44:51 +0000

    feat: Add divider UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 
 import { Hero } from "@/components/Hero";
+import Divider from "@/ui/Divider";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Divider />
     </>
   );
 }
