b5cbdeb0febae8e3753f7438185bf0410e6d5a96
brendino500 <brenda.ty@live.com>
2026-01-21 10:50:08 +0000

    feat: add primary & secondary colours and font

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>HPS 2026</div>
+    <>
+      <div className="text-primary-500 bg-secondary-500 font-primary">HPS 2026</div>
+      <div className="font-secondary">HPS</div>
+    </>
   );
 }
