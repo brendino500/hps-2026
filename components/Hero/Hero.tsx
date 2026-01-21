decaeb9e4eb54e0c387f4dea9e7f9974950fa65f
brendino500 <brenda.ty@live.com>
2026-01-21 11:41:30 +0000

    feat: Add button component

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,10 +1,12 @@
 import Title from "@/ui/Title"
 import Section from "@/ui/Section"
+import Button from "@/ui/Button"
 
 const Hero = () => {
   return (
     <Section id="hero">
       <Title as="h1" text="Heidi Philip-Smith" className="text-7xl" />
+      <Button text="book" type="button" />
     </Section>
   )
 }
